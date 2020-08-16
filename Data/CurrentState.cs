7327e527dffa2f5a4849f390c96fd4c870ee2234
Mark Virchenko <v-vmark@microsoft.com>
2020-08-16 19:52:20 +0300

    init commit

---
+++ Data/CurrentState.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Delivery1.Data
+{
+    public class CurrentState
+    {
+        public int? UserId { get; set; }
+
+        public int AlbumId { get; set; }
+    }
+}
---
+++ HttpClients/DataExplorerClient.cs
@@ -0,0 +1,36 @@
+﻿using Delivery1.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+
+namespace Delivery1.HttpClients
+{
+    public class AClient
+    {
+        private readonly HttpClient client;
+
+        public AClient(HttpClient client)
+        {
+            client.BaseAddress = new Uri("https://jsonplaceholder.typicode.com");
+
+            this.client = client;
+        }
+
+        private IEnumerable<T> GetEntity<T>(string entityName)
+            => client
+                    .GetFromJsonAsync<IEnumerable<T>>(entityName)
+                    .GetAwaiter().GetResult();
+
+        public IEnumerable<Album> GetAlbums(CurrentState state)
+            => GetEntity<Album>("albums").Where(i => i.UserId == state.UserId);
+
+        public IEnumerable<Photo> GetPhotos(CurrentState state)
+            => GetEntity<Photo>("photos").Where(i => i.AlbumId == state.AlbumId);
+
+        public IEnumerable<Comment> GetComments(int photoId)
+            => GetEntity<Comment>("comments").Where(i => i.PostId == photoId);            
+    }
+}
